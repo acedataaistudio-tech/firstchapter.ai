d3c02370b519cfe32a9a9c55df448c3fd8482f2f
Logan Arumugam <logan25arumugam@gmail.com>
2026-05-03 22:53:10 +0530

    Testing3

--- frontend/components/InstitutionDashboard.tsx
+++ frontend/components/InstitutionDashboard.tsx
@@ -162,7 +162,7 @@ console.log('🔍 Loading MAU status for institution:', institutionId);
                         Current capacity and usage of active students
                       </p>
                     </div>
-                    {mauStatus.usage_percent > 0 && (
+                    {mauStatus.usage_percent = 0 && (
                       <button
                         onClick={() => setShowPurchaseModal(true)}
                         style={{
